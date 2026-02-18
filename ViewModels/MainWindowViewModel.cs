dfcfd209aebb79558d39fd71f56a8a2b9424e0ca
c.ramos <clement.ramos@laplateforme.io>
2026-02-18 15:22:14 +0100

    Adding Basic UI layouts to have and navbar

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,36 @@
 ﻿namespace ProjectPlan.ViewModels;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    private readonly DashboardViewModel _dashboard = new();
+    private readonly ProjectViewModel _project = new();
+    private readonly SettingsViewModel _settings = new();
+
+    [ObservableProperty]
+    private ViewModelBase _currentPage;
+
+    public MainWindowViewModel()
+    {
+        _currentPage = _dashboard; // page par défaut
+    }
+
+    [RelayCommand]
+    private void ShowDashboard() => CurrentPage = _dashboard;
+
+    [RelayCommand]
+    private void ShowProject() => CurrentPage = _project;
+
+    [RelayCommand]
+    private void OpenProjectFromCard(DashboardCard card)
+    {
+        _project.ProjectName = card.Title;
+        CurrentPage = _project;
+    }
+
+    [RelayCommand]
+    private void ShowSettings() => CurrentPage = _settings;
 }
